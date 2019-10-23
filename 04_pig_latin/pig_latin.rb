29023b5dad96fd2e8c7a16b0c8d386ca8062feed
Matthew Beilsmith <matthewbeilsmith@gmail.com>
2019-10-23 17:26:45 -0500

    completed pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
-#write your code here
+# frozen_string_literal: true
+
+VOWEL = %w[a e i o u].freeze
+AY = 'ay'
+
+def translate(original)
+  pig_latin = original.split.map do |word|
+    if VOWEL.include?(word[0])
+      word + AY
+    else
+      consonants(word)
+    end
+  end
+  pig_latin.join(' ')
+end
+
+def consonants(word)
+  word = word.split('')
+
+  until VOWEL.include?(word[0])
+    grab_letter = word[0..1].join == 'qu' ? word[0..1].join : word[0]
+    word.shift(grab_letter.length)
+    word << grab_letter
+  end
+  (word << AY).join
+end
