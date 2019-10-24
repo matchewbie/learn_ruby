05e60049ed510b28a4bc2197190867751a25680b
Matthew Beilsmith <matthewbeilsmith@gmail.com>
2019-10-24 04:58:50 -0500

    finished timer with modular arithmetic and helper method

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,21 @@
+# frozen_string_literal: true
+
+# modular arithmetic with helper method
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    second = padded(@seconds % 60)
+    minute = padded(@seconds / 60 % 60)
+    hour = padded(@seconds / 3600 % 60)
+    hour + ':' + minute + ':' + second
+  end
+
+  def padded(num)
+    num.to_s.rjust(2, '0')
+  end
 end
