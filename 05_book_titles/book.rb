ece471e606ce41c9b39954627bee9ade78690fcd
Matthew Beilsmith <matthewbeilsmith@gmail.com>
2019-10-24 02:23:49 -0500

    finished book class

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
+
+# defined predicate method
+class String
+  def do_not_capitalize?
+    %w[the a an and in of].include?(self)
+  end
+end
+
+# title capitalization
 class Book
-# write your code here
+  attr_writer :title
+
+  def title
+    @title.split.map.with_index do |word, i|
+      word.do_not_capitalize? && i != 0 ? word : word.capitalize
+    end.join(' ')
+  end
 end
